2dc6ab161b61d630264cf462b049e44e46363559
Lukáš Krajíček <lukas.krajda211@gmail.com>
2017-05-01 22:29:52 +0200

    code review

--- app/src/main/java/com/example/pota/erazmus_ukol/MainActivity.java
+++ app/src/main/java/com/example/pota/erazmus_ukol/MainActivity.java
@@ -13,32 +13,31 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener {
 
-    Spinner spinner;
-    EditText editText1;
-    EditText editText2;
-    EditText editText3;
+    EditText editText1, editText2, editText3;
     TextView vysledek;
+    Spinner spinner;
+
     Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinner = (Spinner) findViewById(R.id.notebooky);
+        initParams();
+    }
+
+    protected void initParams() {
+        intent = new Intent(this, Activity2.class);
+
         editText1 = (EditText) findViewById(R.id.kusy);
         editText2 = (EditText) findViewById(R.id.cena);
         editText3 = (EditText) findViewById(R.id.smeny);
         vysledek = (TextView) findViewById(R.id.vysledek);
+        spinner = (Spinner) findViewById(R.id.notebooky);
 
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.spinnerTyp, android.R.layout.simple_spinner_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
-    }
-
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
     }
 
     @Override
@@ -49,71 +48,42 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 return;
             }
         }
-        double kusy;
-        double cena;
-        double smeny;
-        double vypocet = 0;
+        double kusy = Float.parseFloat(editText1.getText().toString());
+        double cena = Float.parseFloat(editText2.getText().toString());
+        double smeny = Float.parseFloat(editText3.getText().toString());
+        double vypocet = cena * 0.01 * kusy * smeny;
+
         switch (position) {
             case 1:
-                kusy = Float.parseFloat(editText1.getText().toString());
-                cena = Float.parseFloat(editText2.getText().toString());
-                smeny = Float.parseFloat(editText3.getText().toString());
-                vypocet = cena * 0.01 * kusy * smeny;
-                intent = new Intent(this, Activity2.class);
-                intent.putExtra("vedlejsiAktivita",String.valueOf("Tva mesicni vyplata za notebooky MSI je " + vypocet));
-                startActivity(intent);
+                intent.putExtra("vedlejsiAktivita", String.valueOf("Tva mesicni vyplata za notebooky MSI je " + vypocet));
                 break;
+
             case 2:
-                kusy = Float.parseFloat(editText1.getText().toString());
-                cena = Float.parseFloat(editText2.getText().toString());
-                smeny = Float.parseFloat(editText3.getText().toString());
-                vypocet = cena * 0.01 * kusy * smeny;
-                intent = new Intent(this, Activity2.class);
                 intent.putExtra("vedlejsiAktivita",String.valueOf("Tva mesicni vyplata za notebooky ACER je " + vypocet));
-                startActivity(intent);
                 break;
+
             case 3:
-                kusy = Float.parseFloat(editText1.getText().toString());
-                cena = Float.parseFloat(editText2.getText().toString());
-                smeny = Float.parseFloat(editText3.getText().toString());
-                vypocet = cena * 0.01 * kusy * smeny;
-                intent = new Intent(this, Activity2.class);
                 intent.putExtra("vedlejsiAktivita",String.valueOf("Tva mesicni vyplata za notebooky ASUS je " + vypocet));
-                startActivity(intent);
                 break;
+
             case 4:
-                kusy = Float.parseFloat(editText1.getText().toString());
-                cena = Float.parseFloat(editText2.getText().toString());
-                smeny = Float.parseFloat(editText3.getText().toString());
-                vypocet = cena * 0.01 * kusy * smeny;
-                intent = new Intent(this, Activity2.class);
                 intent.putExtra("vedlejsiAktivita",String.valueOf("Tva mesicni vyplata za notebooky LENOVO je " + vypocet));
-                startActivity(intent);
                 break;
+
             case 5:
-                kusy = Float.parseFloat(editText1.getText().toString());
-                cena = Float.parseFloat(editText2.getText().toString());
-                smeny = Float.parseFloat(editText3.getText().toString());
-                vypocet = cena * 0.01 * kusy * smeny;
-                intent = new Intent(this, Activity2.class);
                 intent.putExtra("vedlejsiAktivita",String.valueOf("Tva mesicni vyplata za notebooky MACKBOOK je " + vypocet));
-                startActivity(intent);
                 break;
+
             case 6:
-                kusy = Float.parseFloat(editText1.getText().toString());
-                cena = Float.parseFloat(editText2.getText().toString());
-                smeny = Float.parseFloat(editText3.getText().toString());
-                vypocet = cena * 0.01 * kusy * smeny;
-                intent = new Intent(this, Activity2.class);
                 intent.putExtra("vedlejsiAktivita",String.valueOf("Tva mesicni vyplata za notebooky DELL je " + vypocet));
-                startActivity(intent);
                 break;
         }
         vysledek.setText("Výsledek je " + vypocet);
+        startActivity(intent);
     }
 
     @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {}
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {}
 }
